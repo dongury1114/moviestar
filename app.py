d9d9a26c68defe37b42db8000558178ac5a16133
donggyu <dongury1114@gmail.com>
2022-02-13 20:16:32 +0900

    2022.02.13

--- app.py
+++ app.py
@@ -1,51 +1,69 @@
-from pymongo import MongoClient
-
 from flask import Flask, render_template, jsonify, request
+import requests
+from bson.objectid import ObjectId
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
-db = client.dbjungle
+client = MongoClient(
+    'mongodb://dongury1114:dlehdrb123@3.34.133.243', 27017)
+db = client.dbjungletest
 
 
-# HTML 화면 보여주기
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-# API 역할을 하는 부분
-@app.route('/api/list', methods=['GET'])
-def show_stars():
-    # 1. db에서 mystar 목록 전체를 검색합니다. ID는 제외하고 like 가 많은 순으로 정렬합니다.
-    # 참고) find({},{'_id':False}), sort()를 활용하면 굿!
-    # 2. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달합니다.
-    stars = list(db.mystar.find({}, {"-id": False}).sort('like', -1))
-    return jsonify({'result': 'success', 'msg': stars})
+# 생성
 
 
-@app.route('/api/like', methods=['POST'])
-def like_star():
-    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
-    name_receive = request.form['namn_give']
-    # 2. mystar 목록에서 find_one으로 name이 name_receive와 일치하는 star를 찾습니다.
-    star = db.mystar.find_one({'name': name_receive})
-    # 3. star의 like 에 1을 더해준 new_like 변수를 만듭니다.
-    new_like = star['like']+1
-    # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
-    # 참고: '$set' 활용하기!
-    db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like}})
-    # 5. 성공하면 success 메시지를 반환합니다.
+@app.route('/memo', methods=['POST'])
+def post_memo():
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+    memo = {'title': title_receive, 'content': content_receive}
+
+    db.memo.insert_one(memo)
 
     return jsonify({'result': 'success'})
 
 
-@app.route('/api/delete', methods=['POST'])
-def delete_star():
-    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
-    # 2. mystar 목록에서 delete_one으로 name이 name_receive와 일치하는 star를 제거합니다.
-    # 3. 성공하면 success 메시지를 반환합니다.
-    return jsonify({'result': 'success', 'msg': 'delete 연결되었습니다!'})
+# 삭제
+@app.route('/memo/delete', methods=['POST'])
+def delete_memo():
+    _id_receive = request.form['_id_give']
+    db.memo.delete_one({'_id': ObjectId(_id_receive)})
+    return jsonify({'result': 'success'})
+
+# 수정
+
+
+@app.route('/memo/revise', methods=['POST'])
+def rivse_memo():
+    memos = []
+    result = list(db.memo.find({}))
+    for memo in result:
+        memo['_id'] = str(memo['_id'])
+        memos.append(memo)
+    _id_receive = request.form['_id_give']
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+    db.memo.update_one({'_id': ObjectId(_id_receive)}, {
+                       '$set': {'title': title_receive}})
+
+    db.memo.update_one({'_id': ObjectId(_id_receive)}, {
+                       '$set': {'content': content_receive}})
+    return jsonify({'result': 'success'})
+
+
+@app.route('/memo/read', methods=['GET'])
+def read_memo():
+    memos = []
+    result = list(db.memo.find({}))
+    for memo in result:
+        memo['_id'] = str(memo['_id'])
+        memos.append(memo)
+    return jsonify({'result': 'success', 'memo': result})
 
 
 if __name__ == '__main__':
